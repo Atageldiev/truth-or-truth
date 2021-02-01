d5e1b8672fc63db3e9f18c8599880e7b6087ed60
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-02-01 19:29:48 +0600

    Encapsulate and split generators logic into functions

--- core/generators.py
+++ core/generators.py
@@ -5,19 +5,22 @@ from core.storage import storage
 
 class GamersGenerator:
     def __init__(self):
-        self._gamers = self.pass_turn()
+        self._generator = self._generate()
+
+    async def _generate(self):
+        for gamer in cycle(await self._get_gamers_list()):
+            yield gamer
 
     @staticmethod
-    async def pass_turn():
-        for gamer in cycle(await storage.get_or_create("gamers", [])):
-            yield gamer
+    async def _get_gamers_list():
+        return await storage.get_or_create("gamers", [])
 
     async def regenerate(self):
         """Regenerates generator"""
-        self._gamers = self.pass_turn()
+        self._generator = self._generate()
 
     async def next(self):
-        return await self._gamers.__anext__()
+        return await self._generator.__anext__()
 
 
 gamers = GamersGenerator()
