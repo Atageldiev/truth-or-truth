55c7f0d46d93480aca3a02b1543253b96c358f42
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-05-09 16:56:37 +0600

    Udpate

--- core/db/manager.py
+++ core/db/manager.py
@@ -1,7 +1,7 @@
-from .base import Database as BaseDatabase
+from .base import Database
 from .tables import CustomQuestionsTable, RatingTable
 
-_db = BaseDatabase("db")
+_db = Database("db")
 
 custom_questions = CustomQuestionsTable(_db.get_connection())
 rating = RatingTable(_db.get_connection())
